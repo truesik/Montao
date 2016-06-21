a7a8205045db65825d67ec39c07e5d7ab0c4b3c9
truesik <truesik@gmail.com>
2016-06-21 18:45:58 +0300

    index controller

---
+++ src/main/java/org/unstoppable/projectstack/controller/IndexController.java
@@ -0,0 +1,19 @@
+package org.unstoppable.projectstack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/")
+public class IndexController {
+    /**
+     * This method is used to open main page.
+     *
+     * @return index.html
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
