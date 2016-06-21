615ed05a927ba883249ff7244b226f00bf274f42
truesik <truesik@gmail.com>
2016-06-22 00:02:46 +0300

    spring dispatcher servlet configuration

---
+++ src/main/java/org/unstoppable/projectstack/config/SpringWebConfig.java
@@ -0,0 +1,58 @@
+package org.unstoppable.projectstack.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+/**
+ * Spring MVC and Thymeleaf configuration.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"org.unstoppable.projectstack.*"})
+public class SpringWebConfig extends WebMvcConfigurerAdapter {
+    private static final String UTF_8 = "UTF-8";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
+        registry.addResourceHandler("/bootstrap/**").addResourceLocations("/WEB-INF/bootstrap/");
+        registry.addResourceHandler("/angularjs/**").addResourceLocations("/WEB-INF/angularjs/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/js/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding(UTF_8);
+        return viewResolver;
+    }
+
+    @Bean
+    public ITemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setEnableSpringELCompiler(true);
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+}
