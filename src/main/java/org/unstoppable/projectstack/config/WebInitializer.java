b802eb2a0226541e18d85c0f8783da9c5864d815
truesik <truesik@gmail.com>
2016-06-22 00:03:38 +0300

    application initializer

---
+++ src/main/java/org/unstoppable/projectstack/config/WebInitializer.java
@@ -0,0 +1,34 @@
+package org.unstoppable.projectstack.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+ * Application initializer.
+ */
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    private static final String UTF_8 = "UTF-8";
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringWebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter(UTF_8);
+        return new Filter[]{encodingFilter};
+    }
+}
