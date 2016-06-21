e8e6867cffd24c4424e60bb84ff539e3c493c125
truesik <truesik@gmail.com>
2016-06-22 00:04:01 +0300

    removed

--- src/main/java/org/unstoppable/projectstack/model/Channel.java
+++
@@ -1,7 +0,0 @@
-package org.unstoppable.projectstack.model;
-
-import java.util.List;
-
-public class Channel {
-    private List<User> userList;
-}
--- src/main/java/org/unstoppable/projectstack/model/Community.java
+++
@@ -1,8 +0,0 @@
-package org.unstoppable.projectstack.model;
-
-import java.util.List;
-
-public class Community {
-    private List<Channel> channelList;
-    private List<User> userList;
-}
