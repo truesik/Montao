e37018d10ad67964fc1eed67e37d00e24e4af337
truesik <truesik@gmail.com>
2016-06-22 22:03:20 +0300

    overrides equals and hashcode

--- src/main/java/org/unstoppable/projectstack/entity/User.java
+++ src/main/java/org/unstoppable/projectstack/entity/User.java
@@ -78,4 +78,29 @@ public class User {
     public void setConfirmed(Boolean isConfirmed) {
         this.isConfirmed = isConfirmed;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (!username.equals(user.username)) return false;
+        if (!password.equals(user.password)) return false;
+        if (!email.equals(user.email)) return false;
+        return isConfirmed.equals(user.isConfirmed);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + username.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + isConfirmed.hashCode();
+        return result;
+    }
 }
