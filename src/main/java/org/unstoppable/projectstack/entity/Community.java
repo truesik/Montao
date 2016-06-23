dd621b27341624d9aa3907afb1163848c95d31c5
truesik <truesik@gmail.com>
2016-06-23 20:00:19 +0300

    community

---
+++ src/main/java/org/unstoppable/projectstack/entity/Community.java
@@ -0,0 +1,70 @@
+package org.unstoppable.projectstack.entity;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Table that contains list of communities.
+ */
+@Entity
+@Table(name = "COMMUNITIES")
+public class Community {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "TITLE", nullable = false)
+    @NotNull
+    @NotEmpty
+    private String title;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Community community = (Community) o;
+
+        if (!id.equals(community.id)) return false;
+        if (!title.equals(community.title)) return false;
+        return description != null ? description.equals(community.description) : community.description == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}
