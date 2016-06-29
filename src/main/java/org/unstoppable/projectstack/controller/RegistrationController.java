3f4b4e9aef9a88ef54515fda4dde4c744a40b323
truesik <truesik@gmail.com>
2016-06-29 19:33:14 +0300

    add get and post methods

--- src/main/java/org/unstoppable/projectstack/controller/RegistrationController.java
+++ src/main/java/org/unstoppable/projectstack/controller/RegistrationController.java
@@ -1,8 +1,11 @@
 package org.unstoppable.projectstack.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.unstoppable.projectstack.entity.User;
+import org.unstoppable.projectstack.service.UserService;
 
 /**
  * Registration.html controller.
@@ -10,8 +13,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/registration")
 public class RegistrationController {
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(method = RequestMethod.GET)
-    public String registration() {
+    public String registration(Model model) {
+        model.addAttribute("user", new User());
         return "registration";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String addUser(@ModelAttribute("user") User user) {
+        user.setRole("ROLE_USER");
+        userService.add(user);
+        return "redirect:/";
     }
 }
