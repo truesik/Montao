0fa8ffae9bae0ad2cc8f4dc33a9e7eeb05a01646
truesik <truesik@gmail.com>
2016-07-09 22:31:28 +0300

    add encoding filter

--- src/main/java/org/unstoppable/projectstack/config/SecurityConfig.java
+++ src/main/java/org/unstoppable/projectstack/config/SecurityConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        http.addFilterBefore(encodingFilter, CsrfFilter.class);
         http.authorizeRequests()
                 .anyRequest().permitAll();
     }
