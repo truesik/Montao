af0e9d730c7ed8023e9c0cc149025380d91efde1
truesik <truesik@gmail.com>
2016-07-10 16:47:22 +0300

    add characters validation in username field

--- src/main/java/org/unstoppable/projectstack/validator/UserValidator.java
+++ src/main/java/org/unstoppable/projectstack/validator/UserValidator.java
@@ -10,8 +10,7 @@ public class UserValidator implements Validator {
 
     public UserValidator(UserService userService) {
         if (userService == null) {
-            throw new IllegalArgumentException("The supplied [Service] is " +
-                    "required and must not be null.");
+            throw new IllegalArgumentException("The supplied [Service] is required and must not be null.");
         }
         this.userService = userService;
     }
@@ -29,12 +28,18 @@ public class UserValidator implements Validator {
     }
 
     private void emailValidation(Errors errors, User user) {
+        // Check email existence
         if (!userService.checkEmail(user.getEmail())) {
             errors.rejectValue("email", "Email already registered.");
         }
     }
 
     private void usernameValidation(Errors errors, User user) {
+        // Only latin chars, numbers and "_"
+        if (!user.getUsername().matches("\\w+")) {
+            errors.rejectValue("username", "Wrong characters in username field.");
+        }
+        // Check username existence
         if (!userService.checkUsername(user.getUsername())) {
             errors.rejectValue("username", "Username already registered.");
         }
