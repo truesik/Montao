1a1816566318d9ff928022ebaeb65d688c8920b6
truesik <truesik@gmail.com>
2016-07-17 20:44:23 +0300

    channel dao interface and implementation

---
+++ src/main/java/org/unstoppable/projectstack/dao/ChannelDAO.java
@@ -0,0 +1,20 @@
+package org.unstoppable.projectstack.dao;
+
+import org.unstoppable.projectstack.entity.Channel;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public interface ChannelDAO {
+    List<Channel> getAll();
+
+    Channel getById(BigInteger id);
+
+    Channel getByTitle(String title);
+
+    void delete(Channel channel);
+
+    void add(Channel channel);
+
+    void update(Channel channel);
+}
---
+++ src/main/java/org/unstoppable/projectstack/dao/ChannelDAOHibernate.java
@@ -0,0 +1,53 @@
+package org.unstoppable.projectstack.dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.unstoppable.projectstack.entity.Channel;
+import org.unstoppable.projectstack.entity.Community;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Repository
+@Transactional
+public class ChannelDAOHibernate implements ChannelDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public List<Channel> getAll() {
+        String hql = "FROM Channel";
+        return sessionFactory.getCurrentSession().createQuery(hql, Channel.class).list();
+    }
+
+    @Override
+    public Channel getById(BigInteger id) {
+        return sessionFactory.getCurrentSession().find(Channel.class, id);
+    }
+
+    @Override
+    public Channel getByTitle(String title) {
+        String hql = "FROM Channel WHERE title = :title";
+        Query<Channel> query = sessionFactory.getCurrentSession().createQuery(hql, Channel.class);
+        query.setParameter("title", title);
+        return query.uniqueResult();
+    }
+
+    @Override
+    public void delete(Channel channel) {
+        sessionFactory.getCurrentSession().delete(channel);
+    }
+
+    @Override
+    public void add(Channel channel) {
+        sessionFactory.getCurrentSession().save(channel);
+    }
+
+    @Override
+    public void update(Channel channel) {
+        sessionFactory.getCurrentSession().update(channel);
+    }
+}
