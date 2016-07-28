74bc0c578e6226955017e6cbf5ff6630f38c1eeb
truesik <truesik@gmail.com>
2016-07-28 19:47:59 +0300

    added method that checks channels existence. fix add channel method

--- src/main/java/org/unstoppable/projectstack/controller/ChannelController.java
+++ src/main/java/org/unstoppable/projectstack/controller/ChannelController.java
@@ -1,16 +1,17 @@
 package org.unstoppable.projectstack.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.unstoppable.projectstack.entity.Channel;
 import org.unstoppable.projectstack.entity.Community;
 import org.unstoppable.projectstack.model.ChannelCreationForm;
 import org.unstoppable.projectstack.service.ChannelService;
 import org.unstoppable.projectstack.service.CommunityService;
+import org.unstoppable.projectstack.validator.ChannelValidator;
 
 import javax.validation.Valid;
 
@@ -18,24 +19,55 @@ import javax.validation.Valid;
 public class ChannelController {
     private final CommunityService communityService;
     private final ChannelService channelService;
+    private final SimpMessagingTemplate messagingTemplate;
 
     @Autowired
-    public ChannelController(CommunityService communityService, ChannelService channelService) {
+    public ChannelController(CommunityService communityService, ChannelService channelService, SimpMessagingTemplate messagingTemplate) {
         this.communityService = communityService;
         this.channelService = channelService;
+        this.messagingTemplate = messagingTemplate;
     }
 
-    @RequestMapping(value = "/channels/new", method = RequestMethod.GET)
-    public String addChannelPage(Model model) {
-        model.addAttribute("channelForm", new ChannelCreationForm());
-        return "newchannel";
+    /**
+     * Creates new channel in current community.
+     *
+     * @param communityTitle Community where channel should be created.
+     * @param channelForm    New channel form.
+     * @param result         Errors.
+     * @return "Success" or "Failure".
+     */
+    @RequestMapping(value = "{communityTitle}/channels/new",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String addChannel(@PathVariable("communityTitle") String communityTitle,
+                             @Valid @RequestBody ChannelCreationForm channelForm,
+                             BindingResult result) {
+        new ChannelValidator(channelService, communityTitle).validate(channelForm, result);
+        // TODO: 23.07.2016 Сделать проверку пользователя, чтобы только авторизованный мог создавать новые каналы
+        // TODO: 23.07.2016 Канал может создать только член данного сообщества
+        if (result.hasErrors()) {
+            return "failure";
+        } else {
+            Community community = communityService.getByTitle(communityTitle);
+            Channel channel = channelForm.createChannel(community);
+            channelService.add(channel);
+            // Broadcast new channel to listeners
+            messagingTemplate.convertAndSend("/topic/" + communityTitle + "/newchannel", channel);
+            return "success";
+        }
     }
 
-    @RequestMapping(value = "/channels/new", method = RequestMethod.POST)
-    public String addChannel(@Valid @ModelAttribute ChannelCreationForm channelForm) {
-        Community community = communityService.getByTitle(communityTitle);
-        Channel channel = channelForm.createChannel(community);
-        channelService.add(channel);
-        return "newchannel";
+    /**
+     * Returns false if channel title already exist and true if not.
+     *
+     * @param communityTitle Community title.
+     * @param title          Channel title.
+     * @return String "true" or "false".
+     */
+    @RequestMapping(value = "{communityTitle}/channels/check_title", method = RequestMethod.POST)
+    @ResponseBody
+    public String checkTitle(@PathVariable("communityTitle") String communityTitle, String title) {
+        return channelService.checkTitle(title, communityTitle).toString();
     }
 }
