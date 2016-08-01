9c6877b6e70c68a74ad4036e1817f07097971938
truesik <truesik@gmail.com>
2016-08-01 23:17:45 +0300

    changed message

--- src/main/webapp/WEB-INF/js/userform-validation.js
+++ src/main/webapp/WEB-INF/js/userform-validation.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
         },
         messages: {
             username: {
-                remote: "This username already exists."
+                remote: "This name already exists."
             },
             email: {
                 remote: "This email already exists."
