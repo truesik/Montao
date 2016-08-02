4c90f071540d785ca7c472e84a5ad84e08af1480
truesik <truesik@gmail.com>
2016-08-02 13:35:54 +0300

    added same username checking

--- src/main/java/org/unstoppable/projectstack/service/CommunityService.java
+++ src/main/java/org/unstoppable/projectstack/service/CommunityService.java
@@ -2,21 +2,28 @@ package org.unstoppable.projectstack.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.unstoppable.projectstack.dao.CommunityDAO;
+import org.unstoppable.projectstack.dao.UserDAO;
 import org.unstoppable.projectstack.entity.Community;
+import org.unstoppable.projectstack.entity.User;
 
 import java.util.List;
 
 public class CommunityService {
     @Autowired
     private CommunityDAO communityDAO;
+    @Autowired
+    private UserDAO userDAO;
 
     public void save(Community community) {
         communityDAO.add(community);
     }
 
     public Boolean checkTitle(String title) {
+        // check community title existence
         Community community = communityDAO.getByTitle(title);
-        return community == null;
+        // check same username existence
+        User user = userDAO.getByUsername(title);
+        return community == null && user == null;
     }
 
     public Community getByTitle(String communityTitle) {
