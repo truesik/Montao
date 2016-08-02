d79505c98f5381c23f5374617c8c306ecc2057b9
truesik <truesik@gmail.com>
2016-08-03 00:07:17 +0300

    added update and send confirm registration message

--- src/main/java/org/unstoppable/projectstack/service/UserService.java
+++ src/main/java/org/unstoppable/projectstack/service/UserService.java
@@ -1,6 +1,9 @@
 package org.unstoppable.projectstack.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 import org.unstoppable.projectstack.dao.CommunityDAO;
 import org.unstoppable.projectstack.dao.UserDAO;
@@ -64,6 +67,10 @@ public class UserService {
         return userDAO.getByUsername(username);
     }
 
+    public User getByToken(String token) {
+        return userDAO.getByUUID(token);
+    }
+
     /**
      * Returns false if user exist and true if not.
      *
@@ -87,5 +94,21 @@ public class UserService {
     public Boolean checkEmail(String email) {
         User user = userDAO.getByEmail(email);
         return user == null;
+    }
+
+    public void update(User user) {
+        userDAO.update(user);
+    }
+
+    public void sendConfirmRegistrationMessage(User user) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(user.getEmail());
+        simpleMailMessage.setSubject("Registration Confirmation");
+        simpleMailMessage.setText("" +
+                "<p>Thanks for signing up!<p>" +
+                "<p>Please click the link below to activate your account:<p>" +
+                "<a href=\"http://localhost:8080/registration/confirm?token=" + user.getUuid() + "\">Verify You Account<a>");
+        JavaMailSender mailSender = new JavaMailSenderImpl();
+        mailSender.send(simpleMailMessage);
     }
 }
