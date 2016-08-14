a6d8b0929ecd74b86f0341903b53b8bb47bfd464
George <zhora.sem@yandex.ru>
2016-08-14 11:24:49 +0300

    Found some forgotten files .

---
+++ src/main/webapp/WEB-INF/templates/model/js/popup-window.js
@@ -0,0 +1,14 @@
+$(function(){ 
+  $('#log-in-button').click(function(){
+     // $('#cover').css('display','block');
+	 // $('#cover').show('slow');
+	 // $('#cover').fadeIn('slow');
+	 $('#cover').slideDown('slow');
+  });
+  $('#btnClose').click(function(){
+	 // $('#cover').css('display','none'); 
+	 // $('#cover').hide('slow');
+	 // $('#cover').fadeOut('slow');
+	 $('#cover').slideUp('slow');
+  });
+});
