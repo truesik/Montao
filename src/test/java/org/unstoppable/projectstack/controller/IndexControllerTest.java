f515e6710d2ecb6db96245ce2720ba6e0f2e445a
truesik <truesik@gmail.com>
2016-08-15 21:39:44 +0300

    added constructor injections commented user verifying

--- src/test/java/org/unstoppable/projectstack/controller/IndexControllerTest.java
+++ src/test/java/org/unstoppable/projectstack/controller/IndexControllerTest.java
@@ -2,20 +2,25 @@ package org.unstoppable.projectstack.controller;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.unstoppable.projectstack.service.CommunityService;
+import org.unstoppable.projectstack.service.UserService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class IndexControllerTest {
+    private CommunityService communityService;
     private MockMvc mockMvc;
 
     @Before
     public void setUp() throws Exception {
-        IndexController controller = new IndexController();
+        communityService = Mockito.mock(CommunityService.class);
+        IndexController controller = new IndexController(communityService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).setViewResolvers(getViewResolver()).build();
     }
 
