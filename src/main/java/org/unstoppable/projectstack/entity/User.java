db0af56f539d4851abb00e6290c656949c78752e
truesik <truesik@gmail.com>
2016-08-17 22:44:41 +0300

    removed unused import

--- src/main/java/org/unstoppable/projectstack/entity/User.java
+++ src/main/java/org/unstoppable/projectstack/entity/User.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.math.BigInteger;
 import java.time.LocalDate;
-import java.util.UUID;
 
 /**
  * Table that contains users.
