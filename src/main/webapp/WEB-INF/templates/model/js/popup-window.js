1c29199be5fd56b3e9df7484b845401db7268bde
George <zhora.sem@yandex.ru>
2016-08-24 08:55:25 +0300

    Modified popup window and create window for creation account.

--- src/main/webapp/WEB-INF/templates/model/js/popup-window.js
+++ src/main/webapp/WEB-INF/templates/model/js/popup-window.js
@@ -4,15 +4,19 @@ $(function(){
 	 // $('#cover').show('slow');
 	 // $('#cover').fadeIn('slow');
 	 $('#cover').slideDown('slow');
+     $('#popup-window').css('display','inherit');
+     $('#create-account-window').css('display','none');
   });
   $('#exit-area').click(function(){
 	 // $('#cover').css('display','none'); 
      // $('#cover').hide('slow');
 	 // $('#cover').fadeOut('slow');
+     
      $('#cover').slideUp('slow');
   });
   $('#create-account-button').click(function(){
-    $('#popup-window').fadeOut('fast');
-      
+    $('#popup-window').fadeOut('fast',function(){
+        $('#create-account-window').fadeIn('fast');
+    });
     });
 });
