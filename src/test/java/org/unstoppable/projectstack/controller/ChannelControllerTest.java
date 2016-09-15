6945965fb6bfb80bd2d49b1e30028d424dc489bb
truesik <truesik@gmail.com>
2016-09-15 23:24:19 +0300

    check title test #51

--- src/test/java/org/unstoppable/projectstack/controller/ChannelControllerTest.java
+++ src/test/java/org/unstoppable/projectstack/controller/ChannelControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.unstoppable.projectstack.entity.Channel;
 import org.unstoppable.projectstack.entity.Community;
+import org.unstoppable.projectstack.entity.Message;
 import org.unstoppable.projectstack.model.ChannelCreationForm;
 import org.unstoppable.projectstack.service.*;
 
@@ -32,7 +33,6 @@ public class ChannelControllerTest {
     private MessageService messageService;
     private SubscriptionService subscriptionService;
     private MockMvc mockMvc;
-
     @Before
     public void setUp() throws Exception {
         communityService = Mockito.mock(CommunityService.class);
@@ -86,6 +86,39 @@ public class ChannelControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    public void checkTitle() throws Exception {
+        String COMMUNITY_TITLE = "communityTest";
+        String CHANNEL_TITLE = "channelTest";
+        Mockito.when(channelService.checkTitle("channelTest", COMMUNITY_TITLE)).thenReturn(true);
+        RequestBuilder request = post("/" + COMMUNITY_TITLE + "/channels/check_title").param("title", CHANNEL_TITLE);
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().string("true"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void channel() throws Exception {
+
+    }
+
+    @Test
+    public void getMessages() throws Exception {
+        String COMMUNITY_TITLE = "communityTest";
+        String CHANNEL_TITLE = "channelTest";
+        Community community = createCommunity();
+        Channel channel = community.getChannels().get(0);
+        Message message = createMessage(channel);
+        List<Message> messages = new ArrayList<>();
+        messages.add(message);
+
+        Mockito.when(communityService.getByTitle(community.getTitle())).thenReturn(community);
+        Mockito.when(messageService.getByChannelWithLimitation(channel, 0, 20)).thenReturn(messages);
+        RequestBuilder request = post("/" + COMMUNITY_TITLE + "/channels/" + CHANNEL_TITLE + "/messages").contentType(MediaType.APPLICATION_JSON_VALUE).param("startRowPosition", "0");
+        mockMvc.perform(request).andExpect(jsonPath("$.id", ))
+    }
+
     private Community createCommunity() {
         Community community = new Community();
         community.setTitle("communityTest");
@@ -109,6 +142,13 @@ public class ChannelControllerTest {
         channelForm.setTitle("channelTest");
         channelForm.setDescription("descriptionTest");
         return channelForm;
+    }
+
+    private Message createMessage(Channel channel) {
+        Message message = new Message();
+        message.setMessage("Hello");
+        message.setChannel(channel);
+        return message;
     }
 
     protected byte[] json(Object o) throws IOException {
