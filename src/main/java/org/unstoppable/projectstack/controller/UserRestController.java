2255465eb93ceb481f2b9624e101eb9067809567
truesik <truesik@gmail.com>
2016-10-05 19:39:10 +0300

    Changed response body, string to response entity

--- src/main/java/org/unstoppable/projectstack/controller/UserRestController.java
+++ src/main/java/org/unstoppable/projectstack/controller/UserRestController.java
@@ -1,17 +1,22 @@
 package org.unstoppable.projectstack.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.unstoppable.projectstack.entity.User;
 import org.unstoppable.projectstack.model.UserRegistrationForm;
 import org.unstoppable.projectstack.service.UserService;
 import org.unstoppable.projectstack.validator.UserValidator;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/api/user")
@@ -24,14 +29,19 @@ public class UserRestController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addUser(@Valid @RequestBody UserRegistrationForm userForm, BindingResult result) {
+    public ResponseEntity<Void> addUser(@Valid @RequestBody UserRegistrationForm userForm,
+                                        BindingResult result,
+                                        UriComponentsBuilder uriComponentsBuilder) {
         new UserValidator(userService).validate(userForm, result);
         if (result.hasErrors()) {
-            return "failure";
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
         } else {
             User user = userForm.createUser();
             userService.registerNewUser(user);
-            return "success";
+            HttpHeaders headers = new HttpHeaders();
+            URI location = uriComponentsBuilder.path("/{username}").buildAndExpand(user.getUsername()).toUri();
+            headers.setLocation(location);
+            return new ResponseEntity<>(headers, HttpStatus.CREATED);
         }
     }
 
