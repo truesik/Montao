b298c25b7366354b7319db43a44713ad67fddc8d
truesik <truesik@gmail.com>
2016-10-09 17:55:40 +0300

    added new get subscribed users

--- src/main/java/org/unstoppable/projectstack/controller/UserRestController.java
+++ src/main/java/org/unstoppable/projectstack/controller/UserRestController.java
@@ -3,29 +3,38 @@ package org.unstoppable.projectstack.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.unstoppable.projectstack.entity.Community;
+import org.unstoppable.projectstack.entity.Subscription;
 import org.unstoppable.projectstack.entity.User;
 import org.unstoppable.projectstack.model.UserRegistrationForm;
+import org.unstoppable.projectstack.service.CommunityService;
+import org.unstoppable.projectstack.service.SubscriptionService;
 import org.unstoppable.projectstack.service.UserService;
 import org.unstoppable.projectstack.validator.UserValidator;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/user")
 public class UserRestController {
     private final UserService userService;
+    private final SubscriptionService subscriptionService;
+    private final CommunityService communityService;
 
     @Autowired
-    public UserRestController(UserService userService) {
+    public UserRestController(UserService userService,
+                              SubscriptionService subscriptionService,
+                              CommunityService communityService) {
         this.userService = userService;
+        this.subscriptionService = subscriptionService;
+        this.communityService = communityService;
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
@@ -53,5 +62,12 @@ public class UserRestController {
     @RequestMapping(value = "/check_email", method = RequestMethod.POST)
     public String checkEmail(String email) {
         return userService.checkEmail(email).toString();
+    }
+
+    @PostMapping(value = "/get_subscribed_users", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Subscription>> getSubscribedUsers(@RequestParam(value = "communityTitle") String communityTitle) {
+        Community community = communityService.getByTitle(communityTitle);
+        List<Subscription> subscriptions = subscriptionService.getByCommunity(community);
+        return new ResponseEntity<>(subscriptions, HttpStatus.OK);
     }
 }
