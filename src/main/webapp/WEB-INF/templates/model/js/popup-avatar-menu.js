2d15e6d6fd1d7605fb33a1e37878f87a65c8740e
GeorgeSemenov <zhora.sem@yandex.ru>
2016-10-10 21:06:43 +0300

    Fix all js scripts. Now they work correct.

---
+++ src/main/webapp/WEB-INF/templates/model/js/popup-avatar-menu.js
@@ -0,0 +1,21 @@
+$(function(){ 
+  $('#avatar-contain').click(function(){
+     // $('#cover').css('display','block');
+	 // $('#cover').show('slow');
+	 // $('#cover').fadeIn('slow');
+	 $('#cover').slideDown('slow');
+     $('#popup-window').css('display','inherit');
+     $('#create-account-window').css('display','none');
+  });
+  $('#exit-area').click(function(){
+	 // $('#cover').css('display','none'); 
+     // $('#cover').hide('slow');
+	 // $('#cover').fadeOut('slow');
+     $('#cover').slideUp('slow');
+  });
+  $('#call-create-account-window-button').click(function(){
+    $('#popup-window').fadeOut('fast',function(){
+        $('#create-account-window').fadeIn('fast');
+        });
+    });
+});
