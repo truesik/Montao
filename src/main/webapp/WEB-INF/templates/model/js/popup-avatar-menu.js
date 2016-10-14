2ec023660be72e1b8e474080c164ff7dd6f0e9d2
GeorgeSemenov <zhora.sem@yandex.ru>
2016-10-14 12:03:26 +0300

    Add animate .

--- src/main/webapp/WEB-INF/templates/model/js/popup-avatar-menu.js
+++ src/main/webapp/WEB-INF/templates/model/js/popup-avatar-menu.js
@@ -1,21 +1,8 @@
-$(function(){ 
+jQuery(document).ready(function(){ 
   $('#avatar-contain').click(function(){
-     // $('#cover').css('display','block');
-	 // $('#cover').show('slow');
-	 // $('#cover').fadeIn('slow');
-	 $('#cover').slideDown('slow');
-     $('#popup-window').css('display','inherit');
-     $('#create-account-window').css('display','none');
-  });
-  $('#exit-area').click(function(){
-	 // $('#cover').css('display','none'); 
-     // $('#cover').hide('slow');
-	 // $('#cover').fadeOut('slow');
-     $('#cover').slideUp('slow');
-  });
-  $('#call-create-account-window-button').click(function(){
-    $('#popup-window').fadeOut('fast',function(){
-        $('#create-account-window').fadeIn('fast');
-        });
-    });
+        $('#popup-menu').animate({"height":"toggle"},
+                                400,
+                                'linear',
+                                function(){})
+  })
 });
