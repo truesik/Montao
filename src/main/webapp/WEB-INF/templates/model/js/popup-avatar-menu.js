0f23610807decbf38f14b97a021fae88e4349b41
GeorgeSemenov <zhora.sem@yandex.ru>
2016-10-15 13:27:50 +0300

    Refactor code for popup-avatar-menu .

--- src/main/webapp/WEB-INF/templates/model/js/popup-avatar-menu.js
+++ src/main/webapp/WEB-INF/templates/model/js/popup-avatar-menu.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function(){ 
   $('#avatar-contain').click(function(){
+        $( "#avatar-contain i" ).toggleClass( "fa-chevron-circle-down" );
+        $( "#avatar-contain i" ).toggleClass( "fa-chevron-circle-up" );
         $('#popup-menu').animate({"height":"toggle"},
                                 500,
                                 'linear',
