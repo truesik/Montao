5ccf334ce667d893052e44939e8a6ea3f2b2c0c3
truesik <truesik@gmail.com>
2016-10-20 21:57:43 +0300

    added messages reducer

--- src/main/webapp/app/reducers/index.js
+++ src/main/webapp/app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import usersReducer from './usersReducer'
 import channelsReducer from "./channelsReducer";
+import messagesReducer from "./messagesReducer";
 
 export default combineReducers({
     usersReducer,
-    channelsReducer
+    channelsReducer,
+    messagesReducer
 })
