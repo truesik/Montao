e636e6caa7657db75d3bae71c25192f2cc74cdf8
truesik <truesik@gmail.com>
2016-10-20 19:28:29 +0300

    es6

--- src/main/webapp/app/components/Channel.js
+++ src/main/webapp/app/components/Channel.js
@@ -1,21 +1,11 @@
 import * as React from "react";
 
-export default class Channel extends React.Component {
-    handleClick(event) {
-        event.preventDefault();
-        console.log("someday... someday")
-    }
+export const Channel = ({channel, onClick}) => {
+    return (
+        <li>
+            <a onClick={() => onClick(channel.title)}>{channel.title}</a>
+        </li>
+    )
+};
 
-    render() {
-        var channel = this.props.channel;
-        var path = $(location).attr('pathname');
-        return (
-            <li>
-                <a
-                    href={`${(path)}/channels/${(channel.title)}`}
-                    onClick={::this.handleClick}
-                >{channel.title}</a>
-            </li>
-        )
-    }
-}
+export default Channel;
