67c208309b1a11ba7c407fb1702f96a3c72475ee
truesik <truesik@gmail.com>
2016-10-20 17:23:05 +0300

    add redux storage provider

--- src/main/webapp/app/index.js
+++ src/main/webapp/app/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './components/App'
+import configureStore from "./store/configureStore";
+
+let store = configureStore();
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
