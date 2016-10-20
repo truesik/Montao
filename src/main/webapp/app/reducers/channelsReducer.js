91661ab0d5c9bd5a0491da9543104212b7c701d2
truesik <truesik@gmail.com>
2016-10-20 17:19:55 +0300

    async reducers

--- src/main/webapp/app/reducers/channelsReducer.js
+++ src/main/webapp/app/reducers/channelsReducer.js
@@ -1,14 +1,28 @@
-import {GET_CHANNELS} from "../constants/Channel";
+import {GET_CHANNELS_REQUEST, GET_CHANNELS_SUCCESS, GET_CHANNELS_FAILURE} from "../constants/Channel";
 const initialState = {
-    channels: []
+    channels: [],
+    error: '',
+    isFetching: false
 };
 
 const channelsReducer = (state = initialState, action) => {
     switch (action.type) {
-        case GET_CHANNELS:
+        case GET_CHANNELS_REQUEST:
             return {
                 ...state,
-                channels: action.payload
+                isFetching: true
+            };
+        case GET_CHANNELS_SUCCESS:
+            return {
+                ...state,
+                channels: action.payload,
+                isFetching: false
+            };
+        case GET_CHANNELS_FAILURE:
+            return {
+                ...state,
+                error: action.payload,
+                isFetching: false
             };
         default:
             return state;
--- src/main/webapp/app/reducers/usersReducer.js
+++ src/main/webapp/app/reducers/usersReducer.js
@@ -1,15 +1,29 @@
-import {GET_USERS} from "../constants/User";
+import {GET_USERS_REQUEST, GET_USERS_SUCCESS, GET_USERS_FAILURE} from "../constants/User";
 
 const initialState = {
-    users: []
+    subscribers: [],
+    error: '',
+    isFetching: false
 };
 
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
-        case GET_USERS:
+        case GET_USERS_REQUEST:
             return {
                 ...state,
-                users: action.payload
+                isFetching: true
+            };
+        case GET_USERS_SUCCESS:
+            return {
+                ...state,
+                subscribers: action.payload,
+                isFetching: false
+            };
+        case GET_USERS_FAILURE:
+            return {
+                ...state,
+                error: action.payload,
+                isFetching: false
             };
         default:
             return state;
