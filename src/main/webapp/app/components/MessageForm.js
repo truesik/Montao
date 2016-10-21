8437463499bce391ee47967e0b6b2531ff7cf460
truesik <truesik@gmail.com>
2016-10-21 22:34:48 +0300

    added handler

--- src/main/webapp/app/components/MessageForm.js
+++ src/main/webapp/app/components/MessageForm.js
@@ -1,11 +1,28 @@
-import * as React from "react";
+import React from "react";
+import ReactDOM from "react-dom";
 
 export default class MessageForm extends React.Component {
+    handleMessageSubmit(event) {
+        if ((event.keyCode == 10 || event.keyCode == 13) && event.ctrlKey) {
+            var messageInput = ReactDOM.findDOMNode(this.refs.messageInput);
+            var currentCommunityTitle = this.props.path.currentCommunityTitle;
+            var currentChannelTitle = this.props.path.currentChannelTitle;
+            if (messageInput.value.trim().length > 0) {
+                var onSubmit = this.props.onSubmit;
+                onSubmit(currentCommunityTitle, currentChannelTitle, messageInput.value);
+                messageInput.value = '';
+            }
+        }
+    }
+
     render() {
         return (
-            <form className="message-input">
-                <input type="text" className="form-control"/>
-            </form>
+            <div className="message-input">
+                <input type="text"
+                       className="form-control"
+                       ref="messageInput"
+                       onKeyDown={this.handleMessageSubmit.bind(this)}/>
+            </div>
         )
     }
 }
