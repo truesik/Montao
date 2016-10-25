b8e8bfa4289f584c59a6fd4f559ac23bd50a73ca
truesik <truesik@gmail.com>
2016-10-25 14:03:11 +0300

    added comments

--- src/main/webapp/app/components/MessageForm.js
+++ src/main/webapp/app/components/MessageForm.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 
 export default class MessageForm extends React.Component {
     handleMessageSubmit(event) {
+        // Check Ctrl + ENTER
         if ((event.keyCode == 10 || event.keyCode == 13) && event.ctrlKey) {
             var messageInput = ReactDOM.findDOMNode(this.refs.messageInput);
             var currentCommunityTitle = this.props.path.currentCommunityTitle;
             var currentChannelTitle = this.props.path.currentChannelTitle;
+            // Check message input value not empty
             if (messageInput.value.trim().length > 0) {
                 var onSubmit = this.props.onSubmit;
+                // Send message
                 onSubmit(currentCommunityTitle, currentChannelTitle, messageInput.value);
+                // Clean up input value
                 messageInput.value = '';
             }
         }
