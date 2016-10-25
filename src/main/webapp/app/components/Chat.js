58e774cb1d3e8cf0bd8910e4aeea65f6c19b2a21
truesik <truesik@gmail.com>
2016-10-25 13:40:26 +0300

    some changes

--- src/main/webapp/app/components/Chat.js
+++ src/main/webapp/app/components/Chat.js
@@ -1,72 +1,22 @@
 import * as React from "react";
-import MessageBox from './MessageBox'
+import MessageBoxContainer from '../containers/MessageBoxContainer'
 import MessageForm from './MessageForm'
 import SideBar from './SideBar'
-import SockJS from 'sockjs-client'
-import $ from 'jquery'
-import {Stomp} from "stompjs/lib/stomp";
 
 export default class Chat extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentChannelTitle: '',
-            stomp: null
-        };
-        this.getLastOpenedChannel = this.getLastOpenedChannel.bind(this);
-        this.connectToWebSocket = this.connectToWebSocket.bind(this);
-    }
-
     componentDidMount() {
-        this.getLastOpenedChannel();
-        // Connect to websocket
-        this.connectToWebSocket();
-        window.ee.addListener('Channel.changed', (channelTitle) => {
-            this.setState({currentChannelTitle: channelTitle})
-        })
-    }
-
-    componentWillUnmount() {
-        window.ee.removeListener('Channel.changed')
-    }
-
-    getLastOpenedChannel() {
-        var csrfToken = csrf;
-        var csrfHeader = 'X-CSRF-TOKEN';
-        var headers = {};
-        headers[csrfHeader] = csrfToken;
-        $.ajax({
-            url: '/api/channel/get_last_opened_channel?communityTitle=' + $(location).attr('pathname').substring(1, $(location).attr('pathname').length),
-            type: 'post',
-            headers: headers,
-            success: function (channel) {
-                this.setState({
-                    currentChannelTitle: channel.title
-                })
-            }.bind(this)
-        });
-    }
-
-    connectToWebSocket() {
-        var sock = new SockJS('/websocket');
-        var stomp = Stomp.over(sock);
-        stomp.connect(
-            'guest',
-            'guest',
-            () => {
-                this.setState({
-                    stomp: stomp
-                })
-            },
-            (error) => {
-                console.log('STOMP: ' + error);
-                setTimeout(this.connectToWebSocket, 10000);
-                console.log('STOMP: Reconnecting in 10 seconds');
-            }
-        )
+        var lastOpenedChannel = this.props.getLastOpenedChannel;
+        lastOpenedChannel();
     }
 
     render() {
+        const currentCommunityTitle = this.props.currentCommunityTitle;
+        const currentChannelTitle = this.props.currentChannelTitle;
+        const sendMessage = this.props.sendMessage;
+        const path = {
+            currentCommunityTitle: currentCommunityTitle,
+            currentChannelTitle: currentChannelTitle
+        };
         return (
             <div className="container-fluid">
                 <div className="row">
@@ -74,8 +24,8 @@ export default class Chat extends React.Component {
                         <SideBar/>
                     </div>
                     <div className="col-md-offset-2 col-md-10">
-                        <MessageBox channel={this.state.currentChannelTitle} webSocket={this.state.stomp}/>
-                        <MessageForm/>
+                        <MessageBoxContainer channel={currentChannelTitle}/>
+                        <MessageForm onSubmit={sendMessage} path={path}/>
                     </div>
                 </div>
             </div>
