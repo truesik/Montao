cf183cd2f0bc4eef2ba77438b91e4f7a28a6fc14
truesik <truesik@gmail.com>
2016-10-25 23:04:55 +0300

    added websocket reducer

--- src/main/webapp/app/reducers/index.js
+++ src/main/webapp/app/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import usersReducer from './usersReducer'
 import channelsReducer from "./channelsReducer";
 import messagesReducer from "./messagesReducer";
+import websocketReducer from "./websocketReducer";
 
 export default combineReducers({
     usersReducer,
     channelsReducer,
-    messagesReducer
+    messagesReducer,
+    websocketReducer
 })
