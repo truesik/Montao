e97c7c329d976f2f2636f1121b5127fd4d1c046c
truesik <truesik@gmail.com>
2016-10-31 19:59:37 +0300

    add actions and state

--- src/main/webapp/app/containers/ChatContainer.js
+++ src/main/webapp/app/containers/ChatContainer.js
@@ -3,17 +3,22 @@ import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import {getLastOpenedChannel} from "../actions/ChannelActions";
 import {sendMessage} from "../actions/MessageActions";
+import {connectToWebsocket, subscribeToTopic, unsubscribe} from "../actions/WebSocketActions";
 
 const mapStateToProps = state => {
     return {
-        currentChannelTitle: state.channelsReducer.currentChannelTitle
+        currentChannelTitle: state.channelsReducer.currentChannelTitle,
+        isConnected: state.websocketReducer.isConnected
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
         getLastOpenedChannel: bindActionCreators(getLastOpenedChannel, dispatch),
-        sendMessage: bindActionCreators(sendMessage, dispatch)
+        sendMessage: bindActionCreators(sendMessage, dispatch),
+        connectToWebSocket: bindActionCreators(connectToWebsocket, dispatch),
+        subscribeToTopic: bindActionCreators(subscribeToTopic, dispatch),
+        unsubscribe: bindActionCreators(unsubscribe, dispatch)
     }
 };
 
