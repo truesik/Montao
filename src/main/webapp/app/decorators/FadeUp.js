6dff8908c23cb3fc3ea1c623fbecd12629618f6d
truesik <truesik@gmail.com>
2016-10-31 19:56:06 +0300

    fade in animation

---
+++ src/main/webapp/app/decorators/FadeUp.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TweenMax from "gsap/src/uncompressed/TweenMax";
+
+const fadeUp = (Component) => {
+    return class FadeUp extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                className: "panel panel-default"
+            }
+        }
+
+        componentWillAppear(callback) {
+            var node = ReactDOM.findDOMNode(this);
+            TweenMax.fromTo(node, 0.6, {opacity: 0.01}, {opacity: 1, ease: Power1.easeIn, onComplete: callback});
+        }
+
+        componentWillEnter(callback) {
+            this.setState({
+                className: "panel panel-danger"
+            });
+            var node = ReactDOM.findDOMNode(this);
+            TweenMax.fromTo(node, 0.6, {opacity: 0.01}, {opacity: 1, ease: Power1.easeIn, onComplete: callback});
+        }
+
+        componentDidEnter() {
+            setTimeout(() => {
+                this.setState({
+                    className: "panel panel-default"
+                })
+            }, 5000);
+        }
+
+        componentWillLeave(callback) {
+            var node = ReactDOM.findDOMNode(this);
+            TweenMax.fromTo(node, 0.6, {opacity: 1}, {opacity: 0.01, onComplete: callback});
+        }
+
+        render() {
+            return (
+                <Component ref="child" {...this.props} className={this.state.className}/>
+            )
+        }
+    }
+};
+
+export default fadeUp;
