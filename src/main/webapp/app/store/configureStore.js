033e0fc58de74ef0712465ca8d29ad6335cf4bcb
truesik <truesik@gmail.com>
2016-10-31 20:24:44 +0300

    added logger and websocket middleware

--- src/main/webapp/app/store/configureStore.js
+++ src/main/webapp/app/store/configureStore.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import reducers from '../reducers';
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import createLogger from "redux-logger";
+import {websocketMiddleware} from "./websocketMiddleware";
 
 export default function configureStore(initialState) {
+    var logger = createLogger();
     return createStore(
         reducers,
         initialState,
-        applyMiddleware(thunk)
+        applyMiddleware(thunk, websocketMiddleware, logger)
     );
 }
