e10af9e03077b82f491c4cad6415ea5f72f81e69
truesik <truesik@gmail.com>
2016-11-01 23:59:11 +0300

    form submit handler

--- src/main/webapp/app/components/AddChannelForm.js
+++ src/main/webapp/app/components/AddChannelForm.js
@@ -9,32 +9,45 @@ export default class AddChannelForm extends React.Component {
         $(node).on('hidden.bs.modal', this.props.handleHideModal)
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        var channelTitle = ReactDOM.findDOMNode(this.refs.channelTitle).value;
+        var channelDescription = ReactDOM.findDOMNode(this.refs.channelDescription).value;
+    }
+
     render() {
         return (
-            <div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel"
+                 aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <button type="button" className="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span
-                                className="sr-only">Close</span></button>
+                            <button type="button" className="close" data-dismiss="modal">
+                                <span aria-hidden="true">&times;</span>
+                                <span className="sr-only">Close</span>
+                            </button>
                             <h4 className="modal-title" id="myModalLabel">New channel</h4>
                         </div>
                         <div className="modal-body">
                             <form method="post" id="new-channel">
                                 <div className="form-group">
                                     <label htmlFor="title">Title</label>
-                                    <input type="text" className="form-control" id="title" name="title" placeholder="Title"/>
+                                    <input type="text" className="form-control" name="title"
+                                           placeholder="Title" ref="channelTitle"/>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="description">Description</label>
-                                    <input type="text" className="form-control" id="description" name="description"
-                                           placeholder="Description"/>
+                                    <input type="text" className="form-control" name="description"
+                                           placeholder="Description" ref="channelDescription"/>
                                 </div>
                             </form>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="submit" className="btn btn-primary" id="create">Create</button>
+                            <button type="submit" className="btn btn-primary" id="create"
+                                    onClick={this.handleSubmit.bind(this)}>
+                                Create
+                            </button>
                         </div>
                     </div>
                 </div>
