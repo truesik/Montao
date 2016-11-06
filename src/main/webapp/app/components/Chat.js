b2451380801b34c731a791c3fca856c760ef8b34
truesik <truesik@gmail.com>
2016-11-06 23:06:45 +0300

    add disconnection on unmount, and community title injection

--- src/main/webapp/app/components/Chat.js
+++ src/main/webapp/app/components/Chat.js
@@ -9,9 +9,14 @@ export default class Chat extends React.Component {
         connectToWebSocket();
     }
 
+    componentWillUnmount() {
+        this.props.unsubscribe();
+        this.props.disconnect();
+    }
+
     componentWillReceiveProps(nextProps) {
         if (this.props.isConnected !== nextProps.isConnected && nextProps.isConnected) {
-            this.props.getLastOpenedChannel();
+            this.props.getLastOpenedChannel(this.props.params.community);
         }
         if (this.props.currentChannelTitle != nextProps.currentChannelTitle && this.props.isConnected) {
             this.props.unsubscribe();
@@ -20,7 +25,7 @@ export default class Chat extends React.Component {
     }
 
     render() {
-        const currentCommunityTitle = this.props.currentCommunityTitle;
+        const currentCommunityTitle = this.props.params.community;
         const currentChannelTitle = this.props.currentChannelTitle;
         const sendMessage = this.props.sendMessage;
         const path = {
@@ -31,10 +36,10 @@ export default class Chat extends React.Component {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-2 sidebar">
-                        <SideBar/>
+                        <SideBar communityTitle={currentCommunityTitle}/>
                     </div>
                     <div className="col-md-offset-2 col-md-10">
-                        <MessageBoxContainer channel={currentChannelTitle}/>
+                        <MessageBoxContainer channel={currentChannelTitle} path={path}/>
                         <MessageForm onSubmit={sendMessage} path={path}/>
                     </div>
                 </div>
