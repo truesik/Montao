b86065233201e880938b80a1cd7f3e0399faae40
truesik <truesik@gmail.com>
2016-11-06 22:51:14 +0300

    add router

--- src/main/webapp/app/index.js
+++ src/main/webapp/app/index.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {Router, Route, IndexRoute, browserHistory} from "react-router";
+import App from './components/App';
+import ChatContainer from "./containers/ChatContainer";
+import CommunityBoxContainer from "./containers/CommunityBoxContainer";
 import configureStore from "./store/configureStore";
 
 let store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={CommunityBoxContainer} />
+                <Route path="community/:community" component={ChatContainer} />
+            </Route>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
