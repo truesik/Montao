c8c0f2a133c79a2763727a201caab75d7adb9428
truesik <truesik@gmail.com>
2016-11-08 17:09:13 +0300

    add join and leave events

--- src/main/webapp/app/reducers/communitiesReducer.js
+++ src/main/webapp/app/reducers/communitiesReducer.js
@@ -21,9 +21,47 @@ const communitiesReducer = (state = initialState, action) => {
                 ...state,
                 error: action.payload
             };
+        case actionTypes.JOIN_REQUEST:
+            return {
+                ...state
+            };
+        case actionTypes.JOIN_SUCCESS:
+            return {
+                ...state,
+                communities: state.communities.map(community => communityReducer(community, action))
+            };
+        case actionTypes.JOIN_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
+        case actionTypes.LEAVE_REQUEST:
+            return {
+                ...state
+            };
+        case actionTypes.LEAVE_SUCCESS:
+            return {
+                ...state,
+                communities: state.communities.map(community => communityReducer(community, action))
+            };
+        case actionTypes.LEAVE_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
         default:
             return state;
     }
+};
+
+const communityReducer = (community, action) => {
+    if (community.id === action.payload.id) {
+        return {
+            ...community,
+            subscribed: action.payload.subscribed
+        }
+    }
+    return community;
 };
 
 export default communitiesReducer;
