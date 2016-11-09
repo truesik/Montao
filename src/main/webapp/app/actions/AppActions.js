bae4c9bbd45400461ed98874eb61fb293868fae3
truesik <truesik@gmail.com>
2016-11-10 00:36:39 +0300

    app actions

---
+++ src/main/webapp/app/actions/AppActions.js
@@ -0,0 +1,33 @@
+import * as actionTypes from "../constants/appConstants";
+
+export const showLogInDialog = () => {
+    return (dispatch) => {
+        dispatch({
+            type: actionTypes.SHOW_LOG_IN_DIALOG
+        })
+    }
+};
+
+export const hideLogInDialog = () => {
+    return (dispatch) => {
+        dispatch({
+            type: actionTypes.HIDE_LOG_IN_DIALOG
+        })
+    }
+};
+
+export const showSignUpDialog = () => {
+    return (dispatch) => {
+        dispatch({
+            type: actionTypes.SHOW_SIGN_UP_DIALOG
+        })
+    }
+};
+
+export const hideSignUpDialog = () => {
+    return (dispatch) => {
+        dispatch({
+            type: actionTypes.HIDE_SIGH_UP_DIALOG
+        })
+    }
+};
