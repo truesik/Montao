030f8e6f108c5d065334b3139c1dd97c0029b50a
truesik <truesik@gmail.com>
2016-11-10 21:13:44 +0300

    navigation bar container

---
+++ src/main/webapp/app/containers/NavBarContainer.js
@@ -0,0 +1,35 @@
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import NavBar from "../components/NavBar";
+import * as userActions from "../actions/UserActions";
+import * as viewActions from "../actions/ViewActions";
+
+const mapStateToProps = (state) => {
+    return {
+        logInDialog: {
+            isShown: state.viewReducer.isShownLogInDialog,
+            isSubmitDisabled: state.viewReducer.isSubmitDisabled
+        },
+        signUpDialog: {
+            isShown: state.viewReducer.isShownSignUpDialog
+        },
+        isAuthorized: state.usersReducer.isAuthorized
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        logInDialogActions: {
+            show: bindActionCreators(viewActions.showLogInDialog, dispatch),
+            hide: bindActionCreators(viewActions.hideLogInDialog, dispatch),
+            logIn: bindActionCreators(userActions.logIn, dispatch)
+        },
+        signUpDialogActions: {
+            show: bindActionCreators(viewActions.showSignUpDialog, dispatch),
+            hide: bindActionCreators(viewActions.hideSignUpDialog, dispatch)
+        },
+        logOut: bindActionCreators(userActions.logOut, dispatch)
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
