7cf9c7925a948bdd94447c88d6edc0351c0cbbab
truesik <truesik@gmail.com>
2016-11-17 23:07:55 +0300

    add check authorization and add user, and add some fixes

--- src/main/webapp/app/actions/UserActions.js
+++ src/main/webapp/app/actions/UserActions.js
@@ -11,23 +11,19 @@ export const getUsers = (communityTitle) => {
         dispatch({
             type: actionTypes.GET_USERS_REQUEST
         });
-        var csrfToken = csrf;
-        var csrfHeader = 'X-CSRF-TOKEN';
-        var headers = {};
-        headers[csrfHeader] = csrfToken;
         $
             .ajax({
                 url: '/api/user/get_subscribed_users?communityTitle=' + communityTitle,
                 type: 'post',
                 headers: headers
             })
-            .then(response => {
+            .then((response, status, xhr) => {
                 dispatch({
                     type: actionTypes.GET_USERS_SUCCESS,
                     payload: response
                 })
             })
-            .fail(error => {
+            .fail((xhr, status, error) => {
                 dispatch({
                     type: actionTypes.GET_USERS_FAILURE,
                     payload: error
@@ -49,13 +45,13 @@ export const logIn = (userCredentials) => {
                 data: data,
                 headers: headers
             })
-            .then(response => {
+            .then((response, status, xhr) => {
                 dispatch({
                     type: actionTypes.LOG_IN_SUCCESS
                 });
                 dispatch(viewActions.hideLogInDialog())
             })
-            .fail(error => {
+            .fail((xhr, status, error) => {
                 dispatch({
                     type: actionTypes.LOG_IN_FAILURE,
                     payload: error
@@ -75,14 +71,69 @@ export const logOut = () => {
                 type: 'post',
                 headers: headers
             })
-            .then(response => {
+            .then((response, status, xhr) => {
                 dispatch({
                     type: actionTypes.LOG_OUT_SUCCESS
                 })
             })
-            .fail(error => {
+            .fail((xhr, status, error) => {
                 dispatch({
                     type: actionTypes.LOG_OUT_FAILURE,
+                    payload: error
+                })
+            })
+    }
+};
+
+export const checkAuthorization = () => {
+    return (dispatch) => {
+        dispatch({
+            type: actionTypes.CHECK_AUTHORIZATION_REQUEST
+        });
+        $
+            .ajax({
+                url: '/api/user/check_authorization',
+                type: 'get'
+            })
+            .then((response, status, xhr) => {
+                dispatch({
+                    type: actionTypes.CHECK_AUTHORIZATION_SUCCESS,
+                    payload: response
+                })
+            })
+            .fail((xhr, status, error) => {
+                dispatch({
+                    type: actionTypes.CHECK_AUTHORIZATION_FAILURE,
+                    payload: error
+                })
+            })
+    }
+};
+
+export const addUser = (user) => {
+    return (dispatch) => {
+        dispatch({
+            type: actionTypes.ADD_USER_REQUEST
+        });
+        $
+            .ajax({
+                url: '/api/user/add',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify(user),
+                headers: headers
+            })
+            .then((response, status, xhr) => {
+                let location = xhr.getResponseHeader('location');
+                dispatch({
+                    type: actionTypes.ADD_USER_SUCCESS,
+                    payload: location
+                });
+                dispatch(viewActions.hideSignUpDialog());
+            })
+            .fail((xhr, status, error) => {
+                dispatch({
+                    type: actionTypes.ADD_USER_FAILUER,
                     payload: error
                 })
             })
