56c7e69afca5ad1d6baa86747b81e357cac630fa
truesik <truesik@gmail.com>
2016-11-24 00:02:04 +0300

    take form out and replace it to its container

--- src/main/webapp/app/components/LogInDialog.js
+++ src/main/webapp/app/components/LogInDialog.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import LogInFormContainer from "../containers/LogInFormContainer";
+
 export default class LogInForm extends React.Component {
     componentDidMount() {
-        var node = ReactDOM.findDOMNode(this);
+        let node = ReactDOM.findDOMNode(this);
         $(node).on('hidden.bs.modal', () => {
             if (this.props.isShown) {
                 this.props.hide();
@@ -22,23 +24,13 @@ export default class LogInForm extends React.Component {
     }
 
     showModal() {
-        var node = ReactDOM.findDOMNode(this);
+        let node = ReactDOM.findDOMNode(this);
         $(node).modal('show');
     }
 
     hideModal() {
-        var node = ReactDOM.findDOMNode(this);
+        let node = ReactDOM.findDOMNode(this);
         $(node).modal('hide');
-    }
-
-    handleLogIn() {
-        let username = ReactDOM.findDOMNode(this.refs.username).value;
-        let password = ReactDOM.findDOMNode(this.refs.password).value;
-        let userCredentials = {
-            username: username,
-            password: password
-        };
-        this.props.logIn(userCredentials);
     }
 
     render() {
@@ -49,7 +41,7 @@ export default class LogInForm extends React.Component {
                  aria-labelledby="myModalLabel"
                  aria-hidden="true">
                 <div className="modal-dialog">
-                    <div className="modal-content">
+                    <div className="modal-content" style={{overflow: 'auto', paddingBottom: '15px'}}>
                         <div className="modal-header">
                             <button type="button" className="close" data-dismiss="modal">
                                 <span aria-hidden="true">&times;</span>
@@ -58,32 +50,7 @@ export default class LogInForm extends React.Component {
                             <h4 className="modal-title" id="myModalLabel">Log In</h4>
                         </div>
                         <div className="modal-body">
-                            <form method="post">
-                                <div className="form-group">
-                                    <label htmlFor="username">Username</label>
-                                    <input type="text"
-                                           className="form-control"
-                                           name="username"
-                                           placeholder="Username"
-                                           ref="username"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password">Password</label>
-                                    <input type="password"
-                                           className="form-control"
-                                           name="password"
-                                           placeholder="Password"
-                                           ref="password"/>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="submit"
-                                    className="btn btn-primary"
-                                    onClick={() => ::this.handleLogIn()}>
-                                Log In
-                            </button>
+                            <LogInFormContainer logIn={this.props.logIn}/>
                         </div>
                     </div>
                 </div>
