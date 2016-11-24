9dd9b373cf1579de58a63a34c39917fb924704e2
truesik <truesik@gmail.com>
2016-11-24 21:48:21 +0300

    renamed

---
+++ src/main/webapp/app/components/AddCommunityDialog.js
@@ -0,0 +1,137 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default class AddCommunityDialog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isVisible: true
+        }
+    }
+    componentDidMount() {
+        const node = ReactDOM.findDOMNode(this);
+        $(node).on('hidden.bs.modal', () => {
+            if (this.props.isShown) {
+                this.props.hide();
+            }
+        })
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.isShown !== nextProps.isShown) {
+            if (nextProps.isShown) {
+                ::this.showModal();
+            } else {
+                ::this.hideModal();
+            }
+        }
+    }
+
+    showModal() {
+        const node = ReactDOM.findDOMNode(this);
+        $(node).modal('show');
+    }
+
+    hideModal() {
+        const node = ReactDOM.findDOMNode(this);
+        $(node).modal('hide');
+    }
+
+    handlePrivacyChange(isVisible) {
+        this.setState({
+            isVisible: isVisible
+        })
+    }
+
+    handleSubmit() {
+        let title = ReactDOM.findDOMNode(this.refs.title).value;
+        let description = ReactDOM.findDOMNode(this.refs.description).value;
+        let founder = ReactDOM.findDOMNode(this.refs.founder).value;
+        let visible = this.state.isVisible;
+        let community = {
+            title: title,
+            description: description,
+            founder: founder,
+            visible: visible
+        };
+        this.props.addCommunity(community);
+    }
+
+    render() {
+        return (
+            <div className="modal fade"
+                 tabIndex="-1"
+                 role="dialog"
+                 aria-labelledby="myModalLabel"
+                 aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal">
+                                <span aria-hidden="true">&times;</span>
+                                <span className="sr-only">Close</span>
+                            </button>
+                            <h4 className="modal-title" id="myModalLabel">New Community</h4>
+                        </div>
+                        <div className="modal-body">
+                            <form method="post">
+                                <div className="form-group">
+                                    <label htmlFor="title">Title</label>
+                                    <input type="text"
+                                           className="form-control"
+                                           name="username"
+                                           placeholder="Title"
+                                           ref="title"/>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="description">Description</label>
+                                    <input type="text"
+                                           className="form-control"
+                                           name="description"
+                                           placeholder="Description"
+                                           ref="description"/>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="founder">Founder</label>
+                                    <input type="text"
+                                           className="form-control"
+                                           name="founder"
+                                           placeholder="Founder"
+                                           ref="founder"
+                                           value={this.props.username}
+                                           readOnly/>
+                                </div>
+                                <div className="radio">
+                                    <label>
+                                        <input type="radio"
+                                               name="visible"
+                                               checked={this.state.isVisible}
+                                               onChange={() => ::this.handlePrivacyChange(true)}/>
+                                        Public
+                                    </label>
+                                </div>
+                                <div className="radio">
+                                    <label>
+                                        <input type="radio"
+                                               name="visible"
+                                               checked={!this.state.isVisible}
+                                               onChange={() => ::this.handlePrivacyChange(false)}/>
+                                        Private
+                                    </label>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="submit"
+                                    className="btn btn-primary"
+                                    onClick={() => ::this.handleSubmit()}>
+                                Create
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
