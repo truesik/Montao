ce90f843edbe5c58baf31253fe6d1a5b0abfc161
truesik <truesik@gmail.com>
2016-11-25 23:52:51 +0300

    replace function to class and add initial values

--- src/main/webapp/app/components/AddCommunityForm.js
+++ src/main/webapp/app/components/AddCommunityForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Field} from "redux-form";
 
-const renderField = ({input, name, label, type, founder, readOnly, meta: {touched, error}}) => {
+const renderField = ({input, name, label, type, readOnly, meta: {touched, error}}) => {
     return (
         <div className={!touched || !error ? "form-group" : "form-group has-error"}>
             <label htmlFor={name}>{label}</label>
@@ -10,53 +10,59 @@ const renderField = ({input, name, label, type, founder, readOnly, meta: {touche
                    type={type}
                    className="form-control"
                    id={name}
-                   value={founder}
                    readOnly={readOnly}/>
             {touched && (error && <small className="help-block">{error}</small>)}
         </div>
     )
 };
 
-const AddCommunityForm = ({error, handleSubmit, pristine, reset, submitting, addCommunity, username}) => {
-    return (
-        <form method="post" onSubmit={handleSubmit(addCommunity)}>
-            <Field name="title"
-                   component={renderField}
-                   type="text"
-                   label="Title"/>
-            <Field name="description"
-                   component={renderField}
-                   type="text"
-                   label="Description"/>
-            <Field name="founder"
-                   component={renderField}
-                   type="text"
-                   label="Founder"
-                   founder={username}
-                   readOnly/>
-            <div className="radio">
-                <label>
-                    <Field name="visible"
-                           component="input"
-                           type="radio"
-                           value="1"
-                           checked/>
-                    Public
-                </label>
-            </div>
-            <div className="radio">
-                <label>
-                    <Field name="visible"
-                           component="input"
-                           type="radio"
-                           value="0"/>
-                    Private
-                </label>
-            </div>
-            {error && <span className="text-danger">{error}</span>}
-            <button type="submit" className="pull-right btn btn-primary" disabled={submitting}>Create</button>
-        </form>
-    )
-};
+export default class AddCommunityForm extends React.Component {
+    componentDidMount() {
+        const initialValues = {
+            'founder': this.props.username,
+            'visible': "true"
+        };
+        this.props.initialize(initialValues);
+    }
 
-export default AddCommunityForm;
+    render() {
+        const {error, handleSubmit, pristine, reset, submitting, addCommunity} = this.props;
+        return (
+            <form method="post" onSubmit={handleSubmit(addCommunity)}>
+                <Field name="title"
+                       component={renderField}
+                       type="text"
+                       label="Title"/>
+                <Field name="description"
+                       component={renderField}
+                       type="text"
+                       label="Description"/>
+                <Field name="founder"
+                       component={renderField}
+                       type="text"
+                       label="Founder"
+                       readOnly/>
+                <div className="radio">
+                    <label>
+                        <Field name="visible"
+                               component="input"
+                               type="radio"
+                               value="true"/>
+                        Public
+                    </label>
+                </div>
+                <div className="radio">
+                    <label>
+                        <Field name="visible"
+                               component="input"
+                               type="radio"
+                               value="false"/>
+                        Private
+                    </label>
+                </div>
+                {error && <span className="text-danger">{error}</span>}
+                <button type="submit" className="pull-right btn btn-primary" disabled={submitting}>Create</button>
+            </form>
+        )
+    }
+};
