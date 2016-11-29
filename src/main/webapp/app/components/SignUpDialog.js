253ac5a80eeaba7e2c2498b9e1af50fffc1ad3ae
truesik <truesik@gmail.com>
2016-11-29 22:19:30 +0300

    replace form to form container

--- src/main/webapp/app/components/SignUpDialog.js
+++ src/main/webapp/app/components/SignUpDialog.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import SignUpFormContainer from "../containers/SignUpFormContainer";
+
 export default class SignUpDialog extends React.Component {
     componentDidMount() {
         const node = ReactDOM.findDOMNode(this);
@@ -31,20 +33,6 @@ export default class SignUpDialog extends React.Component {
         $(node).modal('hide');
     }
 
-    handleSubmit() {
-        let username = ReactDOM.findDOMNode(this.refs.username).value;
-        let email = ReactDOM.findDOMNode(this.refs.email).value;
-        let password = ReactDOM.findDOMNode(this.refs.password).value;
-        let confirmPassword = ReactDOM.findDOMNode(this.refs.confirm_password).value;
-        let user = {
-            username: username,
-            email: email,
-            password: password,
-            confirmPassword: confirmPassword
-        };
-        this.props.signUp(user);
-    }
-
     render() {
         return (
             <div className="modal fade"
@@ -53,7 +41,7 @@ export default class SignUpDialog extends React.Component {
                  aria-labelledby="myModalLabel"
                  aria-hidden="true">
                 <div className="modal-dialog">
-                    <div className="modal-content">
+                    <div className="modal-content" style={{overflow: 'auto', paddingBottom: '15px'}}>
                         <div className="modal-header">
                             <button type="button" className="close" data-dismiss="modal">
                                 <span aria-hidden="true">&times;</span>
@@ -62,48 +50,7 @@ export default class SignUpDialog extends React.Component {
                             <h4 className="modal-title" id="myModalLabel">Registration</h4>
                         </div>
                         <div className="modal-body">
-                            <form method="post">
-                                <div className="form-group">
-                                    <label htmlFor="username">Username</label>
-                                    <input type="text"
-                                           className="form-control"
-                                           name="username"
-                                           placeholder="Username"
-                                           ref="username"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input type="email"
-                                           className="form-control"
-                                           name="email"
-                                           placeholder="Email"
-                                           ref="email"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password">Password</label>
-                                    <input type="password"
-                                           className="form-control"
-                                           name="password"
-                                           placeholder="Password"
-                                           ref="password"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="confirm_password">Confirm Password</label>
-                                    <input type="password"
-                                           className="form-control"
-                                           name="confirm_password"
-                                           placeholder="Confirm Password"
-                                           ref="confirm_password"/>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="submit"
-                                    className="btn btn-primary"
-                                    onClick={() => ::this.handleSubmit()}>
-                                Sign Up
-                            </button>
+                            <SignUpFormContainer signUp={this.props.signUp}/>
                         </div>
                     </div>
                 </div>
