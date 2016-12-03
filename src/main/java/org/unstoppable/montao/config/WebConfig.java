3963d53fbdcd0759d7fa4e3be24204f4896b04e0
truesik <truesik@gmail.com>
2016-12-03 20:29:54 +0300

    removed internationalization and add view controller

--- src/main/java/org/unstoppable/montao/config/WebConfig.java
+++ src/main/java/org/unstoppable/montao/config/WebConfig.java
@@ -1,19 +1,10 @@
 package org.unstoppable.montao.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.ITemplateEngine;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -21,10 +12,6 @@ import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
-import org.unstoppable.montao.formatter.DateFormatter;
-import org.unstoppable.montao.formatter.UserFormatter;
-
-import java.util.Locale;
 
 /**
  * Spring MVC and Thymeleaf configuration.
@@ -35,21 +22,14 @@ import java.util.Locale;
 public class WebConfig extends WebMvcConfigurerAdapter {
     private static final String STANDARD_CHARSET = "UTF-8";
 
-    @Autowired
-    private UserFormatter userFormatter;
-    @Autowired
-    private DateFormatter dateFormatter;
-
     @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addFormatter(userFormatter);
-        registry.addFormatter(dateFormatter);
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
         registry.addResourceHandler("/node_modules/**").addResourceLocations("/node_modules");
         registry.addResourceHandler("/app/**").addResourceLocations("/app/");
         registry.addResourceHandler("/build/**").addResourceLocations("/build/");
@@ -82,27 +62,5 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         // templates to be automatically updated when modified.
         templateResolver.setCacheable(false);
         return templateResolver;
-    }
-
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(new Locale("en"));
-        return localeResolver;
-    }
-
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-        localeChangeInterceptor.setParamName("lang");
-        return localeChangeInterceptor;
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:message");
-        messageSource.setDefaultEncoding(STANDARD_CHARSET);
-        return messageSource;
     }
 }
