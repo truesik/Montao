0e2eeb381b8f89bcd2a196afdaf457a45fc8921c
truesik <truesik@gmail.com>
2016-12-04 21:11:34 +0300

    test helper

---
+++ src/test/java/org/unstoppable/montao/controller/Helper.java
@@ -0,0 +1,77 @@
+package org.unstoppable.montao.controller;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.unstoppable.montao.entity.Channel;
+import org.unstoppable.montao.entity.Community;
+import org.unstoppable.montao.entity.Subscription;
+import org.unstoppable.montao.entity.User;
+import org.unstoppable.montao.model.ChannelCreationForm;
+import org.unstoppable.montao.model.UserRegistrationForm;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Helper {
+    public static UserRegistrationForm createUserForm() {
+        UserRegistrationForm user = new UserRegistrationForm();
+        user.setUsername("username");
+        user.setPassword("password");
+        user.setConfirmPassword("password");
+        user.setEmail("email@mail.com");
+        return user;
+    }
+
+    public static ChannelCreationForm createChannelForm() {
+        ChannelCreationForm form = new ChannelCreationForm();
+        form.setTitle("channelTest");
+        form.setCommunityTitle("communityTest");
+        return form;
+    }
+
+    public static byte[] json(Object o) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return mapper.writeValueAsBytes(o);
+    }
+
+    public static User createUser() {
+        User user = new User();
+        user.setUsername("userTest");
+        user.setEmail("test@test.com");
+        user.setPassword("passwordTest");
+        return user;
+    }
+
+    public static Channel createChannel(Community community) {
+        Channel channel = new Channel();
+        channel.setTitle("channelTest");
+        channel.setCommunity(community);
+        return channel;
+    }
+
+    public static Subscription createSubscription(Community community, User user) {
+        Subscription subscription = new Subscription();
+        subscription.setUser(user);
+        subscription.setCommunity(community);
+        return subscription;
+    }
+
+    public static Community createCommunity() {
+        Community community = new Community();
+        community.setTitle("communityTest");
+        community.setVisible(true);
+        List<Channel> channels = new ArrayList<>();
+        Channel channel = createChannel(community);
+        channels.add(channel);
+        community.setChannels(channels);
+        return community;
+    }
+
+    public static List<Subscription> createSubscriptionList(Subscription subscription) {
+        List<Subscription> subscriptions = new ArrayList<>();
+        subscriptions.add(subscription);
+        return subscriptions;
+    }
+}
