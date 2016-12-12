1e089ce823630bb3953f5d27270d3114c1ddc9ad
truesik <truesik@gmail.com>
2016-12-12 20:43:29 +0300

    refactor to fetch. and add csrf to headers

--- src/main/webapp/app/actions/CommunityActions.js
+++ src/main/webapp/app/actions/CommunityActions.js
@@ -1,36 +1,46 @@
 import * as actionTypes from "../constants/communityConstants";
 import * as viewActions from "./ViewActions";
 import {SubmissionError, reset} from "redux-form";
-
-// var csrfToken = csrf;
-// var csrfHeader = 'X-CSRF-TOKEN';
-// var headers = {};
-// headers[csrfHeader] = csrfToken;
+import {getCookie} from "../utils/cookie";
 
 export const getCommunities = (startRowPosition) => {
     return dispatch => {
         dispatch({
             type: actionTypes.GET_COMMUNITIES_REQUEST
         });
-        $
-            .ajax({
-                url: '/api/community/get_all',
-                type: 'post',
-                data: {startRowPosition: startRowPosition},
-                // headers: headers
+
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
+        headers.append('X-XSRF-TOKEN', getCookie('XSRF-TOKEN'));
+
+        const request = new Request('/api/community/get_all', {
+            method: 'POST',
+            body: `startRowPosition=${startRowPosition}`,
+            headers: headers,
+            credentials: 'same-origin'
+        });
+
+        return fetch(request)
+            .then(response => {
+                if (response.status != 200) {
+                    const error = new Error(response.statusText);
+                    error.response = response;
+                    throw error;
+                }
+                return response.json();
             })
-            .then((communities, status, xhr) => {
+            .then(communities => {
                 dispatch({
                     type: actionTypes.GET_COMMUNITIES_SUCCESS,
                     payload: communities
                 })
             })
-            .fail((xhr, status, error) => {
+            .catch(error => {
                 dispatch({
                     type: actionTypes.GET_COMMUNITIES_FAILURE,
                     payload: error
                 })
-            })
+            });
     }
 };
 
@@ -39,26 +49,39 @@ export const join = (communityTitle) => {
         dispatch({
             type: actionTypes.JOIN_REQUEST
         });
-        $
-            .ajax({
-                url: '/api/community/join',
-                type: "post",
-                cache: false,
-                data: {communityTitle: communityTitle},
-                // headers: headers
+
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
+        headers.append('X-XSRF-TOKEN', getCookie('XSRF-TOKEN'));
+
+        const request = new Request('/api/community/join', {
+            method: 'POST',
+            body: `communityTitle=${communityTitle}`,
+            headers: headers,
+            credentials: 'same-origin'
+        });
+
+        return fetch(request)
+            .then(response => {
+                if (response.status != 200) {
+                    const error = new Error(response.statusText);
+                    error.response = response.json();
+                    throw error;
+                }
+                return response.json();
             })
-            .then((response, status, xhr) => {
+            .then(response => {
                 dispatch({
                     type: actionTypes.JOIN_SUCCESS,
                     payload: response
                 })
             })
-            .fail((xhr, status, error) => {
+            .catch(error => {
                 dispatch({
                     type: actionTypes.JOIN_FAILURE,
                     payload: error
                 })
-            })
+            });
     }
 };
 
@@ -67,26 +90,39 @@ export const leave = (communityTitle) => {
         dispatch({
             type: actionTypes.LEAVE_REQUEST
         });
-        $
-            .ajax({
-                url: '/api/community/leave',
-                type: "post",
-                cache: false,
-                data: {communityTitle: communityTitle},
-                // headers: headers
+
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
+        headers.append('X-XSRF-TOKEN', getCookie('XSRF-TOKEN'));
+
+        const request = new Request('/api/community/leave', {
+            method: 'POST',
+            body: `communityTitle=${communityTitle}`,
+            headers: headers,
+            credentials: 'same-origin'
+        });
+
+        return fetch(request)
+            .then(response => {
+                if (response.status != 200) {
+                    const error = new Error(response.statusText);
+                    error.response = response;
+                    throw error;
+                }
+                return response.json();
             })
-            .then((response, status, xhr) => {
+            .then(response => {
                 dispatch({
                     type: actionTypes.LEAVE_SUCCESS,
                     payload: response
                 })
             })
-            .fail((xhr, status, error) => {
+            .catch(error => {
                 dispatch({
                     type: actionTypes.LEAVE_FAILURE,
                     payload: error
                 })
-            })
+            });
     }
 };
 
@@ -98,7 +134,7 @@ export const add = (community) => {
 
         const headers = new Headers();
         headers.append('Content-Type', 'application/json; charset=utf-8');
-
+        headers.append('X-XSRF-TOKEN', getCookie('XSRF-TOKEN'));
         const request = new Request('/api/community/add', {
             method: 'POST',
             body: JSON.stringify(community),
@@ -144,7 +180,7 @@ export const checkSubscription = (communityTitle) => {
 
         const headers = new Headers();
         headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
-
+        headers.append('X-XSRF-TOKEN', getCookie('XSRF-TOKEN'));
         const request = new Request('/api/community/check_subscription', {
             method: 'POST',
             body: `communityTitle=${communityTitle}`,
