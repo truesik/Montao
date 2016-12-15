cac9db6cdf157541eac5fe6c177be6711158b20e
truesik <truesik@gmail.com>
2016-12-15 22:17:34 +0300

    now it work

--- src/main/webapp/app/reducers/messagesReducer.js
+++ src/main/webapp/app/reducers/messagesReducer.js
@@ -1,4 +1,4 @@
-import {fromJS, Map} from 'immutable';
+import {fromJS} from 'immutable';
 
 import * as constants from '../constants/messageConstants';
 
@@ -12,21 +12,21 @@ const initialState = fromJS({
 const messagesReducer = (state = initialState, action) => {
     switch (action.type) {
         case constants.GET_MESSAGES_REQUEST:
-            return state.get('messages').clear();
+            return state.update('messages', messages => messages.clear());
         case constants.GET_MESSAGES_SUCCESS:
             return state
                 .set('messages', fromJS(action.payload))
                 .set('startRowPosition', action.payload.length)
-                .set('scrollPosition', true);
+                .set('scrollBottom', true);
         case constants.GET_MESSAGES_FAILURE:
             return state.set('error', action.payload);
         case constants.GET_MESSAGE_SUCCESS:
-            return state.get('messages').push(Map(action.payload));
+            return state.update('messages', messages => messages.push(fromJS(action.payload)));
         case constants.GET_OLDEST_MESSAGES_REQUEST:
             return state;
         case constants.GET_OLDEST_MESSAGES_SUCCESS:
             return state
-                .update('messages', messages => messages.unshift(fromJS(action.payload)))
+                .update('messages', messages => fromJS(action.payload).concat(messages))
                 .update('startRowPosition', count =>  count + action.payload.length)
                 .set('scrollBottom', false);
         case constants.GET_OLDEST_MESSAGES_FAILURE:
