05e01695453190570f39c331006e922cc31c0bc1
truesik <truesik@gmail.com>
2016-12-17 21:23:08 +0300

    some changes

--- src/main/webapp/app/reducers/channelsReducer.js
+++ src/main/webapp/app/reducers/channelsReducer.js
@@ -1,10 +1,10 @@
-import {fromJS} from 'immutable';
+import {fromJS, Map} from 'immutable';
 
 import * as constants from '../constants/channelConstants';
 
 const initialState = fromJS({
     channels: [],
-    currentChannelTitle: '',
+    currentChannel: Map(),
     error: '',
     channelPath: ''
 });
@@ -20,11 +20,11 @@ const channelsReducer = (state = initialState, action) => {
         case constants.GET_LAST_OPENED_CHANNEL_REQUEST:
             return state;
         case constants.GET_LAST_OPENED_CHANNEL_SUCCESS:
-            return state.set('currentChannelTitle', action.payload);
+            return state.set('currentChannel', Map(action.payload));
         case constants.GET_LAST_OPENED_CHANNEL_FAILURE:
             return state.set('error', action.payload);
         case constants.SET_CURRENT_CHANNEL:
-            return state.set('currentChannelTitle', action.payload);
+            return state.set('currentChannel', Map(action.payload));
         case constants.ADD_CHANNEL_REQUEST:
             return state;
         case constants.ADD_CHANNEL_SUCCESS:
