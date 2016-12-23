67d2231b95d6a1800fdf2b9318aa5d5ff642bb8f
truesik <truesik@gmail.com>
2016-12-23 23:33:59 +0300

    decorator

---
+++ src/main/webapp/app/decorators/Checker.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const checker = (Component) => {
+    return class extends React.Component {
+        componentDidMount() {
+            this.props.checkCommunityExistence(this.props.params.community);
+        }
+
+        render() {
+            let communityTemplate;
+            if (this.props.valid && !this.props.notFound) {
+                communityTemplate = (
+                    <Component {...this.props}/>
+                )
+            } else if (!this.props.valid && this.props.notFound) {
+                communityTemplate = (
+                    <NotFound/>
+                )
+            } else if (!this.props.valid) {
+                communityTemplate = (
+                    <div className="loading">
+                        <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+                    </div>
+                )
+            }
+            return (
+                <div>
+                    {communityTemplate}
+                </div>
+            )
+        }
+    }
+};
+
+export default checker;
