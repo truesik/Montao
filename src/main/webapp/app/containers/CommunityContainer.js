0bdeaf087e1c84b9c2cf6346728b0f89a3a33d4c
truesik <truesik@gmail.com>
2017-02-01 20:43:35 +0300

    community container

--- src/main/webapp/app/containers/CommunityContainer.js
+++ src/main/webapp/app/containers/CommunityContainer.js
@@ -1,19 +1,38 @@
-import {connect} from 'react-redux';
-import {bindActionCreators} from "redux";
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
 
 import Community from '../components/Community';
 import * as communityActions from '../actions/CommunityActions';
+import * as viewActions from "../actions/ViewActions";
+import * as userActions from "../actions/UserActions";
 
 const mapStateToProps = (state) => {
     return {
         community: state.communitiesReducer.get('currentCommunity'),
-        valid: state.communitiesReducer.get('valid')
+        valid: state.communitiesReducer.get('valid'),
+        notFound: state.communitiesReducer.get('notFound'),
+        channelListDialog: {
+            isShown: state.viewReducer.get('isShownChannelListDialog')
+        },
+        userListDialog: {
+            isShown: state.viewReducer.get('isShownUserListDialog')
+        },
+        subscribers: state.usersReducer.get('subscribers').toArray()
     }
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        checkCommunityExistence: bindActionCreators(communityActions.checkCommunity, dispatch)
+        checkCommunityExistence: bindActionCreators(communityActions.checkCommunity, dispatch),
+        channelListDialogActions: {
+            show: bindActionCreators(viewActions.showChannelListDialog, dispatch),
+            hide: bindActionCreators(viewActions.hideChannelListDialog, dispatch)
+        },
+        userListDialogActions: {
+            show: bindActionCreators(viewActions.showUserListDialog, dispatch),
+            hide: bindActionCreators(viewActions.hideUserListDialog, dispatch)
+        },
+        getUsers: bindActionCreators(userActions.getUsers, dispatch)
     }
 };
 
