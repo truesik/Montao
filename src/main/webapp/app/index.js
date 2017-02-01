c7172a5ad3a78120e3d230202ca8109a9fad6593
truesik <truesik@gmail.com>
2017-02-01 20:41:07 +0300

    new routes

--- src/main/webapp/app/index.js
+++ src/main/webapp/app/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
+import configureStore from "./store/configureStore";
+
 import AppContainer from './containers/AppContainer';
 import ChatContainer from "./containers/ChatContainer";
 import CommunityThumbnailBoxContainer from "./containers/CommunityThumbnailBoxContainer";
 import CommunityContainer from "./containers/CommunityContainer";
 import NotFound from "./components/NotFound";
-import configureStore from "./store/configureStore";
+import ChannelContainer from "./containers/ChannelContainer";
 
 let store = configureStore();
 
@@ -18,7 +20,9 @@ ReactDOM.render(
             <Route path="/" component={AppContainer}>
                 <IndexRoute component={CommunityThumbnailBoxContainer}/>
                 <Route path="community/:community" component={CommunityContainer}/>
-                <Route path="community/:community/channel/:channel" component={ChatContainer}/>
+                <Route path="community/:community/channel" component={ChatContainer}>
+                    <Route path=":channel" component={ChannelContainer}/>
+                </Route>
                 <Route path="*" component={NotFound}/>
             </Route>
         </Router>
