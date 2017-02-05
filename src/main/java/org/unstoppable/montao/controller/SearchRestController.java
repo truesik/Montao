0160b3145b3f07908e46410081ad26a218d61195
truesik <truesik@gmail.com>
2017-02-05 20:50:12 +0300

    change request address

--- src/main/java/org/unstoppable/montao/controller/SearchRestController.java
+++ src/main/java/org/unstoppable/montao/controller/SearchRestController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/search")
+@RequestMapping(value = "/api/search")
 public class SearchRestController {
     private final CommunityService communityService;
 
