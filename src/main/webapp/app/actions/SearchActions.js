6a310fbe0f2ed63ee33641fdfa331b3ef8202baa
GeorgeSemenov <zhora.sem@yandex.ru>
2017-02-10 20:12:01 +0300

    ADD SOME actions.

--- src/main/webapp/app/actions/SearchActions.js
+++ src/main/webapp/app/actions/SearchActions.js
@@ -1,3 +1,21 @@
-export const search = (query) => {
+import * as actionTypes from "../constants/searchConstants";
 
+export const search = (query) => {
+    const SEARCH_REQUEST = new Request('.../api/search?q=myCommunityTitle', {
+        method: 'POST',
+        body: `myCommuniyTitle=${communityTitle}`,
+        headers: headers,
+        credentials: 'same-origin'
+    });
+    return fetch(SEARCH_REQUEST)
+        .then(response => {
+            dispatch({
+                type:constants.SEARCH_SUCCESS
+            })
+        })
+        .catch(error =>{
+            dispatch({
+                type:constants.SEARCH_FAILURE
+            })
+        })
 }
