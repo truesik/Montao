d1cdd8bb214bde01567c0a9882dc2fd7f0d47325
GeorgeSemenov <zhora.sem@yandex.ru>
2017-02-11 20:10:57 +0300

    We do it with Slava.

--- src/main/webapp/app/actions/SearchActions.js
+++ src/main/webapp/app/actions/SearchActions.js
@@ -1,21 +1,40 @@
 import * as actionTypes from "../constants/searchConstants";
+import {getCookie} from "../utils/cookie";
 
 export const search = (query) => {
-    const SEARCH_REQUEST = new Request('.../api/search?q=myCommunityTitle', {
-        method: 'POST',
-        body: `myCommuniyTitle=${communityTitle}`,
-        headers: headers,
-        credentials: 'same-origin'
-    });
-    return fetch(SEARCH_REQUEST)
-        .then(response => {
-            dispatch({
-                type:constants.SEARCH_SUCCESS
+    return (dispatch) => {
+        dispatch({
+            type: actionTypes.SEARCH_REQUEST
+        });
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded; charset=utf-8');
+        headers.append('X-XSRF-TOKEN', getCookie('XSRF-TOKEN'));
+        const request = new Request('/api/search', {
+            method: 'POST',
+            body: `q=${query}`,
+            headers: headers,
+            credentials: 'same-origin'
+        });
+        return fetch(request)
+            .then(response => {
+                if(response.status != 200){
+                    const error = new Error(response.statusText);
+                    error.response = response;
+                    throw error;
+                }
+                return response.json()
             })
-        })
-        .catch(error =>{
-            dispatch({
-                type:constants.SEARCH_FAILURE
+            .then(response => {
+                dispatch({
+                    type: actionTypes.SEARCH_SUCCESS,
+                    payload: response
+                })
             })
-        })
+            .catch(error => {
+                dispatch({
+                    type: actionTypes.SEARCH_FAILURE,
+                    payload: error
+                })
+            })
+    }
 }
