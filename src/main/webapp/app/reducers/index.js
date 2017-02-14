307f47bed7be165300338eb95766e17df82a1969
truesik <truesik@gmail.com>
2017-02-14 23:21:06 +0300

    clean up

--- src/main/webapp/app/reducers/index.js
+++ src/main/webapp/app/reducers/index.js
@@ -1,11 +1,11 @@
-import { combineReducers } from "redux";
-import {reducer as formReducer} from "redux-form";
+import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import usersReducer from './usersReducer'
-import channelsReducer from "./channelsReducer";
-import messagesReducer from "./messagesReducer";
-import websocketReducer from "./websocketReducer";
-import communitiesReducer from "./communitiesReducer";
-import viewReducer from "./viewReducer";
+import channelsReducer from './channelsReducer';
+import messagesReducer from './messagesReducer';
+import websocketReducer from './websocketReducer';
+import communitiesReducer from './communitiesReducer';
+import viewReducer from './viewReducer';
 
 export default combineReducers({
     usersReducer,
