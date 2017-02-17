9739c7a67547064e94bbc4998c2a65aa8dc6ab3d
GeorgeSemenov <zhora.sem@yandex.ru>
2017-02-17 21:51:38 +0300

    Work almost done.

---
+++ src/main/webapp/app/reducers/searchReducer.js
@@ -0,0 +1,21 @@
+import {fromJS} from 'immutable';
+
+import * as constants from '../constants/searchConstants';
+
+const initialState = fromJS({
+    result: [],
+    error:''
+})
+
+const searchReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case constants.SEARCH_REQUEST:
+            return state;
+        case constants.SEARCH_SUCCESS:
+            return state.set('result', fromjs(action.payload));
+        case constants.SEARCH_FAILURE:
+            return state.set('error', action.payload);
+    }
+};
+
+export default searchReducer;
