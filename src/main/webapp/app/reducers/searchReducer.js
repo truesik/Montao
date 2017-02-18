306e931fb43201e0261e75004f00c04d4ee5b099
GeorgeSemenov <zhora.sem@yandex.ru>
2017-02-18 20:33:46 +0300

    push it baby.

--- src/main/webapp/app/reducers/searchReducer.js
+++ src/main/webapp/app/reducers/searchReducer.js
@@ -4,15 +4,15 @@ import * as constants from '../constants/searchConstants';
 
 const initialState = fromJS({
     result: [],
-    error:''
-})
+    error: ''
+});
 
 const searchReducer = (state = initialState, action) => {
     switch (action.type) {
         case constants.SEARCH_REQUEST:
             return state;
         case constants.SEARCH_SUCCESS:
-            return state.set('result', fromjs(action.payload));
+            return state.set('result', fromJS(action.payload));
         case constants.SEARCH_FAILURE:
             return state.set('error', action.payload);
     }
