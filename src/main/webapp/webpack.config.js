4ac66df7e87af9389a96cf410463858d120e3745
truesik <truesik@gmail.com>
2017-02-20 19:10:29 +0300

    Переформатировал и добавил загрузчик eslint

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -1,52 +1,60 @@
-const {resolve} = require('path');
+const { resolve } = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:4200',
-        'webpack/hot/only-dev-server',
-        './app/index.js'
-    ],
-    output: {
-        path: resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        hot: true,
-        contentBase: resolve(__dirname, 'build'),
-        port: 4200,
-        publicPath: '/',
-        historyApiFallback: true,
-        proxy: {
-            "/api": "http://localhost:3000"
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    'babel-loader',
-                ],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader'
-                ]
-            }
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:4200',
+    'webpack/hot/only-dev-server',
+    './app/index.js'
+  ],
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'build'),
+    port: 4200,
+    publicPath: '/',
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: [
+          'eslint-loader'
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new HtmlWebpackPlugin({
-            template: "index.html"
-        })
+      },
+      {
+        test: /\.js$/,
+        loader: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader'
+        ]
+      }
     ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
 };
