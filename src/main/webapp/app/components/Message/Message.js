75b794cfdcdf97c05a0cd93f33c53e338b01035c
truesik <truesik@gmail.com>
2017-02-24 00:59:45 +0300

    Выпилил ReactTransitionGroup который занимался анимацией.

--- src/main/webapp/app/components/Message/Message.js
+++ src/main/webapp/app/components/Message/Message.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Map } from 'immutable';
 
-import fadeUp from '../../decorators/FadeUp';
-
-@fadeUp
 export default class Message extends React.Component {
   render() {
     const message = this.props.message;
     return (
-      <div id={message.get('id')} className={this.props.className}>
+      <div id={message.get('id')} className='panel panel-default'>
         <div className="panel-body">
           <strong>{message.getIn(['user', 'username'])}</strong>
           <small>{message.get('receivedTime')}</small>
