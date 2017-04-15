da5efe9d92b1cb6751c1a9de4b76fddfbfe4cef6
GeorgeSemenov <zhora.sem@yandex.ru>
2017-04-15 14:38:22 +0300

    Реализовал же.

--- src/main/java/org/unstoppable/montao/dao/MessageDAOJpa.java
+++ src/main/java/org/unstoppable/montao/dao/MessageDAOJpa.java
@@ -1,7 +1,37 @@
 package org.unstoppable.montao.dao;
 
-/**
- * Created by User on 15.04.2017.
- */
-public class MessageDAOJpa {
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.unstoppable.montao.entity.Channel;
+import org.unstoppable.montao.entity.Message;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional
+public class MessageDAOJpa implements MessageDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void add(Message message) {entityManager.persist(message);}
+
+    @Override
+    public void delete(Message message) {entityManager.remove(message);}
+
+    @Override
+    public void update(Message message) {entityManager.merge(message);}
+
+    @Override
+    public List<Message> getByChannel(Channel channel) {
+        String jpql = "FROM Message where channel= :channel";
+        return entityManager.createQuery(jpql,Message.class).getResultList();
+    }
+
+    @Override
+    public List<Message> getByChannelWithLimitation(Channel channel, int startRowPosition, int maxResult) {
+        return null;
+    }
 }
